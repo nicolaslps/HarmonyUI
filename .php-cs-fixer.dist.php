5024ea25a8a1e2eab4153e3c1faba9150bd642ca
nicolaslps <lopesn.pro@gmail.com>
2026-07-01 01:20:53 +0200

    Add php cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = new PhpCsFixer\Finder()
+    ->in([
+        __DIR__.'/apps/doc/src',
+        __DIR__.'/packages/core/src',
+    ])
+    ->exclude([
+        'tests',
+        'Tests',
+        'vendor',
+        'var',
+    ]);
+
+return new PhpCsFixer\Config()
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        '@PHP8x4Migration' => true,
+        '@PHP8x4Migration:risky' => true,
+        'declare_strict_types' => true,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+    ])
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__.'/.php-cs-fixer.cache');
