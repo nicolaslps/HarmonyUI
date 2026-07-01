2fa028dabeb7f8e1351f7a303a6a4612752707a3
nicolaslps <lopesn.pro@gmail.com>
2026-07-01 16:27:10 +0200

    Integrate SymfonyCasts Tailwind Bundle and configure Tailwind CSS

--- apps/doc/config/bundles.php
+++ apps/doc/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     HarmonyUI\Core\HarmonyUICoreBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
