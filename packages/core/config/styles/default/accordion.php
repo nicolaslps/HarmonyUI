ec5d6df6107d645901325d6d10cd04988b36f60e
nicolaslps <lopesn.pro@gmail.com>
2026-07-03 19:09:43 +0200

    Add PHP configuration for HarmonyUI `Accordion` and `Button` components

---
+++ packages/core/config/styles/default/accordion.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use HarmonyUI\Core\Style\ComponentStyle;
+
+return [
+    'accordion' => new ComponentStyle(base: 'hui-accordion'),
+    'accordion-item' => new ComponentStyle(base: 'hui-accordion__item'),
+    'accordion-header' => new ComponentStyle(base: 'hui-accordion__header'),
+    'accordion-trigger' => new ComponentStyle(base: 'hui-accordion__trigger'),
+    'accordion-indicator' => new ComponentStyle(base: 'hui-accordion__indicator'),
+    'accordion-panel' => new ComponentStyle(base: 'hui-accordion__panel'),
+    'accordion-panel-body' => new ComponentStyle(base: 'hui-accordion__panel-body'),
+];
