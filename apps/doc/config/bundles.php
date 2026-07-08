4c1c65a99c54832dcbe9788507a6d67558373caf
nicolaslps <lopesn.pro@gmail.com>
2026-07-08 19:22:50 +0200

    Integrate `WebProfilerBundle` for improved development debugging tools

--- apps/doc/config/bundles.php
+++ apps/doc/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- apps/doc/config/reference.php
+++ apps/doc/config/reference.php
@@ -887,6 +887,14 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         post_processors?: array<string, array<string, mixed>>,
  *     },
  * }
+ * @psalm-type WebProfilerConfig = array{
+ *     toolbar?: bool|array{ // Profiler toolbar configuration
+ *         enabled?: bool|Param, // Default: false
+ *         ajax_replace?: bool|Param, // Replace toolbar on AJAX requests // Default: false
+ *     },
+ *     intercept_redirects?: bool|Param, // Default: false
+ *     excluded_ajax_paths?: scalar|Param|null, // Default: "^/((index|app(_[\\w]+)?)\\.php/)?_wdt"
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -911,6 +919,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         stimulus?: StimulusConfig,
  *         ux_icons?: UxIconsConfig,
  *         liip_imagine?: LiipImagineConfig,
+ *         web_profiler?: WebProfilerConfig,
  *     },
  *     "when@prod"?: array{
  *         imports?: ImportsConfig,
@@ -937,6 +946,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         stimulus?: StimulusConfig,
  *         ux_icons?: UxIconsConfig,
  *         liip_imagine?: LiipImagineConfig,
+ *         web_profiler?: WebProfilerConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
